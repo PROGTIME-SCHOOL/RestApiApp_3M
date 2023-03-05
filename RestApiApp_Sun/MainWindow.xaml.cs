3c6cff29b2d2f22b9e6abf16844ce0134f66ae46
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2023-03-05 17:29:15 +0300

    Commit 2

---
+++ RestApiApp_Sun/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using RestApiApp_Sun.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestApiApp_Sun
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SunResultModel model = await LoadSunInfo();
+
+            lblSunrise.Content = "Info Sunrise: " + model.Results.Sunrise.ToLocalTime();
+            lblSunset.Content = "Info Sunset: " + model.Results.Sunset.ToLocalTime();
+        }
+
+        private async Task<SunResultModel> LoadSunInfo()
+        {
+            return await SunProcessor.LoadSunInfo(55.809062f, 37.622382f);
+        }
+    }
+}
---
+++ RestApiApp_Sun/SunProcessor.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RestApiApp_Sun.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiApp_Sun
+{
+    public static class SunProcessor
+    {
+        private static HttpClient client = new HttpClient();
+
+        public static async Task<SunResultModel> LoadSunInfo(float lat = 0, float lng = 0)
+        {
+            string url = $"https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}";
+
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            {
+                string responceString = await response.Content.ReadAsStringAsync();
+
+                SunResultModel sunResultModel = 
+                    JsonConvert.DeserializeObject<SunResultModel>(responceString);
+
+                return sunResultModel;
+            }
+        }
+    }
+}
